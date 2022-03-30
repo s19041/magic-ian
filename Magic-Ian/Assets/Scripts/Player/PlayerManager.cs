b9b5ac146656eed116e3092ca70e188e98ff5ac8
s20715 <s20715@pjwstk.edu.pl>
2022-03-30 13:07:38 +0200

    Implemented player data in playerManager for future in-game progress and added a scroll option to deck display

--- Magic-Ian/Assets/Scripts/Player/PlayerManager.cs
+++ Magic-Ian/Assets/Scripts/Player/PlayerManager.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int gold;
+    public int tutorialRuns;
+    public int succesfulRuns;
+    public int failedRuns;
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<Rank> unlockedRanks;
+    public List<Suit> unlockedSuits;
     private static PlayerManager _instance;
 
     public static PlayerManager Instance { get { return _instance; } }
@@ -31,6 +29,41 @@ public class PlayerManager : MonoBehaviour
             _instance = this;
         }
         DontDestroyOnLoad(gameObject);
+        unlockedRanks = new List<Rank>();
+        unlockedSuits = new List<Suit>();
+        gold = 0;
+        tutorialRuns = 0;
+        succesfulRuns = 0;
+        failedRuns = 0;
+    }
+    public void NewGame()
+    {
+        unlockedRanks.Add(Rank.THREE);
+        unlockedRanks.Add(Rank.FOUR);
+        unlockedRanks.Add(Rank.FIVE);
+        unlockedRanks.Add(Rank.SIX);
+        unlockedRanks.Add(Rank.SEVEN);
 
+        unlockedSuits.Add(Suit.HEARTS);
+        unlockedSuits.Add(Suit.CLUBS);
+    }
+    public bool UnlockCard(Card card)
+    {
+        if (unlockedRanks.Contains(card.rank))
+            return false;
+        
+        
+        if ( card.suit == Suit.SPECIAL)
+            UnlockSuit(Suit.SPECIAL);
+        unlockedRanks.Add(card.rank);
+        return true;
+
+    }
+    public bool UnlockSuit(Suit suit)
+    {
+        if (unlockedSuits.Contains(suit))
+            return false;
+        unlockedSuits.Add(suit);
+        return true;
     }
 }
