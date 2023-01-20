62bd21b9707dece11b686dad3edc79956ae1cf9a
s20715 <s20715@pjwstk.edu.pl>
2023-01-20 19:41:49 +0100

    Audio fix to singleton

--- Magic-Ian/Assets/DontDestroyAudio.cs
+++ Magic-Ian/Assets/DontDestroyAudio.cs
@@ -6,6 +6,17 @@ public class DontDestroyAudio : MonoBehaviour
 {
     private void Awake()
     {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
         DontDestroyOnLoad(transform.gameObject);
     }
+
+    private static DontDestroyAudio _instance;
 }
